63112f28df71896198bd455149f03dd2d662996a
Lancelot220 <inovyj3@gmail.com>
2024-10-28 23:15:24 +0200

    less lightning data, settings saving

--- Assets/Scripts/UI/Settings/GPSettings.cs
+++ Assets/Scripts/UI/Settings/GPSettings.cs
@@ -1,16 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Localization.Settings;
 
 public class GPSettings : MonoBehaviour
 {
     //LANGUAGE
+    public TMP_Dropdown langDropdown;
     public void ChangeLang(int index)
     { StartCoroutine(ChangeLang_(index)); }
     IEnumerator ChangeLang_(int index)
     {
         yield return LocalizationSettings.InitializationOperation;
         LocalizationSettings.SelectedLocale = LocalizationSettings.AvailableLocales.Locales[index];
+        PlayerPrefs.SetInt("selectedlanguageIndex", index);
+    }
+    void Awake()
+    {
+        //LANGUAGE
+        StartCoroutine(ChangeLang_(PlayerPrefs.GetInt("selectedlanguageIndex")));
+        var selectedLocale = LocalizationSettings.SelectedLocale;
+        var locales = LocalizationSettings.AvailableLocales.Locales;
+        int localeIndex = 0;
+        for (int i = 0; i< locales.Count; i++) if (locales[i] == selectedLocale) localeIndex = i;
+        langDropdown.value = localeIndex;
     }
 }
--- Assets/Scripts/UI/Settings/SoundSettings.cs
+++ Assets/Scripts/UI/Settings/SoundSettings.cs
@@ -2,22 +2,41 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.Localization.SmartFormat.Utilities;
+using UnityEngine.UI;
 
 public class SoundSettings : MonoBehaviour
 {
     public AudioMixer mixer;
+    public Slider master;
+    public Slider sfx;
+    public Slider music;
+
+    void Awake()
+    {
+        master.value = PlayerPrefs.GetFloat("masterVolume", 1);
+        sfx.value = PlayerPrefs.GetFloat("sfxVolume", 1);
+        music.value = PlayerPrefs.GetFloat("musicVolume", 1);
+
+        ChangeMaster(master.value);
+        ChangeSFX(sfx.value);
+        ChangeMusic(music.value);
+    }
     public void ChangeMaster(float level)
     {
         mixer.SetFloat("Master", Mathf.Log10(level) * 20f);
+        PlayerPrefs.SetFloat("masterVolume", level);
     }
 
     public void ChangeSFX(float level)
     {
         mixer.SetFloat("SFX", Mathf.Log10(level) * 20f);
+        PlayerPrefs.SetFloat("sfxVolume", level);
     }
 
     public void ChangeMusic(float level)
     {
         mixer.SetFloat("Music", Mathf.Log10(level) * 20f);
+        PlayerPrefs.SetFloat("musicVolume", level);
     }
 }
